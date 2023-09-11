71ba192eb42526ae224b42395f5f6167fc537ab3
Eytan Levy <eslevy17@gmail.com>
2023-09-11 08:48:04 -0700

    Add revert feature to note editor section

--- src/routes/[collectionID]/[noteID]/+page.svelte
+++ src/routes/[collectionID]/[noteID]/+page.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
     import { enhance } from '$app/forms';
+    import revert from '$lib/icons/revert.svg';
     import save from '$lib/icons/save.svg';
+    import {invalidateAll} from "$app/navigation";
 
     export let data;
 </script>
 
 <h4>{data.note.title}</h4>
 <form method="POST" action="?/update-content" use:enhance>
-    <textarea name="content">{data.note.content}</textarea>
+    <textarea name="content" bind:value={data.note.content}></textarea>
     <label>
         <img src="{save}" alt="save" />
         <input type="submit" value="Submit" />
+    </label>
+
+    <label>
+        <img src="{revert}" alt="revert" />
+        <button on:click|preventDefault={() => invalidateAll()}>Revert</button>
     </label>
 </form>
 
@@ -20,6 +27,7 @@
         height: 1rem;
     }
 
+    label button,
     label input[type="submit"] {
         display: none;
     }
